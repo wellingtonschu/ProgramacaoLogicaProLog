8753f01a2bdf43ee0b4896242088a74017d5b482
Wellington Schütz <wellington.schu@live.com>
2016-11-16 11:10:41 -0200

    Correção Ex04.pl

--- Exercicios/Lista03/Ex04.pl
+++ Exercicios/Lista03/Ex04.pl
@@ -1,9 +1,11 @@
 %4
-calculos([X|Xs],min2,max2):-
+calculos([X|Xs],Maior,Menor,Media):-
     max([X|Xs],K),
     min([X|Xs],L),
-    max2 = K,
-    min2 = L.
+    media([X|Xs],M),
+	Maior = K,
+    Menor = L,
+    Media= M.
 
 max([X],X).
 max([X,Y|Cauda],Max):-
@@ -16,7 +18,18 @@ max([_,Y|Cauda],Max):-
 min([X],X).
 min([X,Y|Cauda],Min):-
     X=<Y,
-    !,
-    min([X|Cauda],Min).
+    !
+    ,min([X|Cauda],Min).
 min([_,Y|Cauda],Min):-
     min([Y|Cauda],Min).
+
+tam_lista([_], 1):-
+    !.
+tam_lista([_|L], T):-
+    tam_lista(L, X),
+    T is X + 1.
+
+media(L,M):-
+    soma_lista(L, S),
+    tam_lista(L,T),
+    M is S / T.
